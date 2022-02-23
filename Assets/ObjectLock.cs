95cea2376342b052aa8e5358ecc7ecc3dba82c88
Maximilian <max.helmersson@gmail.com>
2022-02-23 11:04:52 +0100

    Added lock on all objects

--- Assets/ObjectLock.cs
+++ Assets/ObjectLock.cs
@@ -7,32 +7,37 @@ public class ObjectLock : MonoBehaviour
 
     public Vector3 shipLockPosition;
     public Vector3 lockBoxSize;
-    public GameObject moveableShip;
-    public Transform shipPrefab;
+    public GameObject lockedGameObject;
+    public GameObject[] unlockedGameObjects;
+    public GameObject[] lockedGameObjects;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //lockedGameObject.transform.position = shipLockPosition;
+        checkComponent(unlockedGameObjects[0], lockedGameObjects[0]);
+        //Instantiate(shipPrefab, shipLockPosition, Quaternion.identity);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (moveableShip != null) {
-            checkComponent();
+        for (int i = 0; i < unlockedGameObjects.Length; i++) {
+            if (unlockedGameObjects[i] != null) {
+                checkComponent(unlockedGameObjects[i], lockedGameObjects[i]);
+            }
         }
-        
     }
 
-    void checkComponent() {
-        Vector3 shipPosition = moveableShip.transform.position;
-        if (shipPosition.x > shipLockPosition.x - lockBoxSize.x && shipPosition.x < shipLockPosition.x + lockBoxSize.x) {
-            if (shipPosition.y > shipLockPosition.y - lockBoxSize.y && shipPosition.y < shipLockPosition.y + lockBoxSize.y) {
-                if (shipPosition.z > shipLockPosition.z - lockBoxSize.z && shipPosition.z < shipLockPosition.z + lockBoxSize.z) {
+    void checkComponent(GameObject unlockedGameObject, GameObject lockedGameObject) {
+        Vector3 objectPosition = unlockedGameObject.transform.position;
+        if (objectPosition.x > shipLockPosition.x - lockBoxSize.x && objectPosition.x < shipLockPosition.x + lockBoxSize.x) {
+            if (objectPosition.y > shipLockPosition.y - lockBoxSize.y && objectPosition.y < shipLockPosition.y + lockBoxSize.y) {
+                if (objectPosition.z > shipLockPosition.z - lockBoxSize.z && objectPosition.z < shipLockPosition.z + lockBoxSize.z) {
                     Debug.Log("WITHIN AREA!!");
-                    Destroy(moveableShip);
-                    Instantiate(shipPrefab, shipLockPosition, Quaternion.identity);
+                    Destroy(unlockedGameObject);
+                    lockedGameObject.GetComponent<MeshRenderer>().enabled = true;
                 }
             }
         }
